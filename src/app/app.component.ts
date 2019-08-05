4a546aecbf5bebbc2a6edee8fbeb5983b787a8ce
ulacit <ulacit@docente.ulacit.ac.cr>
2019-08-05 16:30:35 -0600

    Primer Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+//Imports Firebase
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { Observable } from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'acme-root',
@@ -6,5 +10,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Semana12Prof';
+  title = 'Semana 10';
+
+  title_section = 'Hola mundo';
+
+  itemsRef: AngularFireList<any>;
+  items: Observable<any[]>;
+
+  constructor(db: AngularFireDatabase) {
+    this.itemsRef = db.list('/contactos');
+
+    //Use snapshotChanges().map() to store the key
+
+    this.items = this.itemsRef.snapshotChanges().pipe(
+      map(action =>
+        action.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+      )
+
+    )
+  }
+
+
+
 }
+
+
+
+
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
+//Firebase Conection
+export const firebaseConfig = {
+  apiKey: "AIzaSyAHsaEUyrgwZYd-v_cgjLW0yDw-gypIUMo",
+  authDomain: "semana12-b1ba9.firebaseapp.com",
+  databaseURL: "https://semana12-b1ba9.firebaseio.com",
+  storageBucket: "semana12-b1ba9.appspot.com",
+  messagingSenderId: "920915323552"
+};
+
 
 @NgModule({
   declarations: [
@@ -10,7 +22,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    AngularFireModule.initializeApp(firebaseConfig), 
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
